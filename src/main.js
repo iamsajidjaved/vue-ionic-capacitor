bba72f6ead8b983ef7a137cb26f8896876b7e3c2
maliksajid <engr.maliksajidkhan@gmail.com>
2019-10-06 00:18:21 +0500

    components created with routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import  HomeComponent  from  './components/HomeComponent.vue'
+import  AboutComponent  from  './components/AboutComponent.vue'
+
+//import vue-router
+import  Router  from  'vue-router'
+Vue.use(Router)
 
 // import ionic
 import IonicVue from '@ionic/vue';
@@ -7,8 +13,24 @@ import '@ionic/core/css/core.css';
 import '@ionic/core/css/ionic.bundle.css';
 Vue.use(IonicVue);
 
+const  router  =  new  Router({
+  routes: [
+  {
+  path:  '/',
+  name:  'HomeComponent',
+  component:  HomeComponent
+  },
+  {
+  path:  '/AboutComponent',
+  name:  'AboutComponent',
+  component:  AboutComponent
+  }
+  ]
+  })
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
