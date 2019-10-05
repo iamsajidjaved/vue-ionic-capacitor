b9cc2c5e7dd91e870ea6ecdadd68f388b65c1e1e
maliksajid <engr.maliksajidkhan@gmail.com>
2019-10-06 00:06:36 +0500

    ionic, vue-router & ionicons are installed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import ionic
+import IonicVue from '@ionic/vue';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+Vue.use(IonicVue);
+
 Vue.config.productionTip = false
 
 new Vue({
